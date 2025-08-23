969a2da4b739baed48b0642193e3407f4d47e231
Mina Hermina <mina.albert.saeed@gmail.com>
2025-08-24 00:13:46 +0300

    refactor(string): Clean-up unnecessary comments

--- string.h
+++ string.h
@@ -42,200 +42,35 @@ typedef struct {
     size_t capacity;
 } String;
 
-/* Init a String object with a C string  */
-/**
- * _str_init:
- * @string: a #String object
- * @init_str: (nullable): a null-terminated C string to initialize with, or %NULL
- *
- * Initializes a String object with the contents of a C string.
- * If @init_str is %NULL, the string will be initialized as empty.
- *
- */
+void _str_init(String *string, const char *init_str, Args args);
+
 #define str_init(string, init_str, ...) \
     _str_init(string, init_str, (Args){ __VA_ARGS__})
 
-void _str_init(String *string, const char *init_str, Args args);
-
-/**
- * str_insert_cstr_at:
- * @string: a #String object
- * @pos: position to insert at (0-based index)
- * @init_str: a null-terminated C string to insert
- *
- * Inserts a C string at the specified position in the String object.
- * All characters at and after @pos will be shifted to the right.
- *
- */
+void _str_insert_cstr_at(String *string, size_t pos, const char *init_str, Args arg);
 
 #define str_insert_cstr_at(string, pos, init_str, ...) \
     _str_insert_cstr_at(string, pos, init_str, (Args){__VA_ARGS__})
 
-void _str_insert_cstr_at(String *string, size_t pos, const char *init_str, Args arg);
 
-/**
- * str_set_cstr:
- * @string: a #String object
- * @cstr: a null-terminated C string
- *
- * Sets the contents of the String object to the given C string,
- * replacing any existing content.
- *
- */
+void str_set(String *dest, const String *src);
 void str_set_cstr(String *string, const char *cstr);
 
-/**
- * str_append_cstr:
- * @string: a #String object
- * @init_str: a null-terminated C string to append
- *
- * Appends the contents of a C string to the end of the String object.
- *
- */
+void _str_append_cstr(String *string, const char *init_str, Args args);
 
 #define str_append_cstr(string, init_str, ...) \
     _str_append_cstr(string, init_str, (Args){__VA_ARGS__})
 
-void _str_append_cstr(String *string, const char *init_str, Args args);
-
-/**
- * str_insert_at:
- * @string: a #String object
- * @pos: position to insert at (0-based index)
- * @src: a #String object to insert
- *
- * Inserts the contents of @src at the specified position in @string.
- * All characters at and after @pos will be shifted to the right.
- *
- */
 void str_insert_at(String *string, size_t pos, String *src);
-
-/**
- * str_set_at:
- * @string: a #String object
- * @index: the position of the character to set (0-based)
- * @ch: the character to set
- *
- * Sets the character at the specified index to the given character.
- * The @index must be within the current bounds of the string.
- *
- */
 void str_set_at(String *string, size_t index, const char ch);
-
-/**
- * str_append:
- * @dest: destination #String object
- * @src: source #String object to append
- *
- * Appends the contents of @src to the end of @dest.
- *
- */
 void str_append(String *dest, String *src);
-
-/**
- * str_set:
- * @dest: destination #String object
- * @src: source #String object
- *
- * Sets the contents of @dest to be a copy of @src,
- * replacing any existing content in @dest.
- *
- */
-void str_set(String *dest, const String *src);
-
-/**
- * str_substr:
- * @dest: destination #String object
- * @src: source #String object
- * @pos: starting position in @src (0-based index)
- * @length: number of characters to extract
- *
- * Extracts a substring from @src and stores it in @dest.
- * The substring starts at @pos and contains @length characters.
- * If @pos + @length exceeds the length of @src, only the available
- * characters are extracted.
- *
- */
 void str_substr(String *dest, const String *src, size_t pos, size_t length, Arena *arena);
-
-/**
- * str_reverse:
- * @string: a #String object
- *
- * Reverses the contents of the string in-place.
- *
- */
 void str_reverse(const String *string);
-
-/**
- * str_lower:
- * @string: a #String object
- *
- * Converts all characters in the string to lowercase in-place.
- * Only ASCII characters are affected.
- *
- */
 void str_lower(String *string);
-
-/**
- * str_upper:
- * @string: a #String object
- *
- * Converts all characters in the string to uppercase in-place.
- * Only ASCII characters are affected.
- *
- */
 void str_upper(String *string);
-
-/**
- * str_at:
- * @string: a #String object
- * @index: the position of the character to retrieve (0-based)
- *
- * Retrieves the character at the specified index.
- *
- * Returns: the character at @index, or '\0' if @index is out of bounds
- *
- */
 char str_at(const String *string, size_t index);
-
-/**
- * str_compare:
- * @string1: first #String object
- * @string2: second #String object
- *
- * Compares two strings lexicographically (case-sensitive).
- *
- * Returns: an integer less than, equal to, or greater than zero if
- *          @string1 is found, respectively, to be less than, to match,
- *          or to be greater than @string2
- *
- */
 int str_compare(const String *string1, const String *string2);
-
-/**
- * str_icompare:
- * @string1: first #String object
- * @string2: second #String object
- *
- * Compares two strings lexicographically in a case-insensitive manner.
- *
- * Returns: an integer less than, equal to, or greater than zero if
- *          @string1 is found, respectively, to be less than, to match,
- *          or to be greater than @string2 (ignoring case)
- *
- */
 int str_icompare(const String *string1, const String *string2);
-
-/**
- * str_free:
- * @string: a #String object
- *
- * Frees the memory allocated for the String object and sets it to
- * an empty state. After calling this function, the string can be
- * reused with str_init() or other functions.
- *
- */
 void str_free(String *string);
 
 #endif
