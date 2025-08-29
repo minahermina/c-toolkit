81bf7aeafa1e391b9410a8faed838cc52088270b
Mina Hermina <mina.albert.saeed@gmail.com>
2025-08-29 03:02:04 +0300

    Edit main.c

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ read_file_to_string(String *string, const char *filename)
     #define SIZE (4096)
     char buf[SIZE] = {0};
 
+    string->size = 0;
     if (string == NULL) {
         fprintf(stderr, "Invalid string pointer for file %s, %s\n", filename, strerror(errno));
         return -1;
@@ -32,13 +33,12 @@ read_file_to_string(String *string, const char *filename)
         return -1;
     }
 
-    /* TODO: add str_append_cstr_n to support add n bytes from the cstr to str */
     while ((read_bytes = read(fd, buf, SIZE)) > 0) {
-        str_append_cstr(string, buf);
+        str_append_cstr_n(string, buf, read_bytes);
     }
 
     if (read_bytes < 0) {
-        fprintf(stderr, "Error reading the file %s\n", filename);
+        fprintf(stderr, "Error reading from the file %s\n", filename);
         close(fd);
         return -1;
     }
@@ -51,16 +51,23 @@ int
 main()
 {
     Arena arena = {0};
-    String str = {0};
+    String str1 = {0};
+    String str2 = {0};
     arena_init(&arena, ARENA_REGION_DEFAULT_CAPACITY);
 
-    str_copy_cstr(&str, "MinaMina", .arena=&arena);
-    // str_append_cstr(&str, "\nAge: 22", .arena=&arena);
-    arena_dump(&arena);
-    // str_append_cstr(&str, "\nGender: Male", .arena=&arena);
+    str_set_cstr(&str1, "Mina", .arena=&arena);
+    str_set_cstr(&str2, "Albert", .arena=&arena);
+    // str_substr(&str1, &str2, 0, 2, .arena=&arena);
+    // str.size = 0;
+    // read_file_to_string(&str, "/home/mina/.config/nvim/lua/mappings.lua");
 
-    printf("str is: \n" STR_FMT "\n", STR_ARG(str));
-    debug_string(&str);
+    printf(STR_FMT"\n", STR_ARG(str1));
+    // printf(STR_FMT"\n", STR_ARG(str2));
+    // printf("-----------------");
+    // arena_dump(&arena);
+    debug_string(&str1);
+    debug_string(&str2);
+    // printf("%ld", (str2.arr - str1.arr));
     // str_append_cstr(&str, ";Age: 22");
     // str_append_cstr(&str, " test string", .arena = &arena);
     // str_append_cstr(&str, "MINA Albert Saeed");
@@ -68,7 +75,7 @@ main()
     // str_append_cstr(&str, "MINA Albert Saeed");
 
     // printf("str is: " STR_FMT "\n", STR_ARG(str));
-    arena_destroy(&arena);
+    // arena_destroy(&arena);
 
     return 0;
 }
