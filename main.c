bd339f580d76b44c1a3c7901b580c4e2c91b8b17
Mina Hermina <mina.albert.saeed@gmail.com>
2025-09-04 12:36:29 +0300

    Edit main.c
    Signed-off-by: Mina Hermina <mina.albert.saeed@gmail.com>

--- main.c
+++ main.c
@@ -1,10 +1,4 @@
-#include <pthread.h>
 #include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <ctype.h>
-#include <errno.h>
 #include "string.h"
 #include "arena.h"
 
@@ -13,42 +7,6 @@
     printf("Capacity: %0zu\n", (str)->capacity);\
     printf("Address: %5p\n", (str)->arr);\
 } \
-
-#define read_file_to_string(string, filename, ...) \
-    _read_file_to_string(string, filename, (Args){__VA_ARGS__})
-int
-_read_file_to_string(String *string, const char *filename, Args args)
-{
-    int fd;
-    ssize_t read_bytes;
-    #define SIZE (4096)
-    char buf[SIZE] = {0};
-
-    string->size = 0;
-    if (string == NULL) {
-        fprintf(stderr, "Invalid string pointer for file %s, %s\n", filename, strerror(errno));
-        return -1;
-    }
-
-    fd = open(filename, O_RDONLY);
-    if (fd < 0) {
-        fprintf(stderr, "Error opening the file %s, %s\n", filename, strerror(errno));
-        return -1;
-    }
-
-    while ((read_bytes = read(fd, buf, SIZE)) > 0) {
-        str_append_cstr_n(string, buf, read_bytes, .arena=args.arena);
-    }
-
-    if (read_bytes < 0) {
-        fprintf(stderr, "Error reading from the file %s\n", filename);
-        close(fd);
-        return -1;
-    }
-
-    close(fd);
-    return 0;
-}
 
 int
 main()
